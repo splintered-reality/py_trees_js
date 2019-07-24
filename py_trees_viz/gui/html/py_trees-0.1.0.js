3b30d1893d8b7b1de54c790cc1992ee6e41a16f4
Daniel Stonier <d.stonier@gmail.com>
2019-07-24 11:50:18 -0400

    [js] no need to keep _nodes, _links around

--- py_trees_viz/gui/html/py_trees-0.1.0.js
+++ py_trees_viz/gui/html/py_trees-0.1.0.js
@@ -7,7 +7,7 @@
 //   Originally had these in py_trees.shapes, however that replaces
 //   (doesn't combine with) joint.shapes, resulting in a few inconsistent
 //   behaviours when I do call on joint.shapes components (ostensibly
-//   because their views don't get found). 
+//   because their views don't get found).
 // *************************************************************************
 
 var joint = joint
@@ -411,14 +411,16 @@ var py_trees = (function() {
   // *************************************************************************
 
   var _version = '0.1.0'
-  var _links = []
-  var _nodes = {}
-  var _text_blocks = []
+
+  var _foo = function({all_the_things}) {
+    console.log("Inside: " + all_the_things)
+    all_the_things.push(5)
+    console.log("Inside: " + all_the_things)
+  }
 
   var _update_graph = function({graph, tree}) {
     console.log("Adding tree to the graph")
-    _nodes = {}
-    _links = []
+    var _nodes = {}
     for (behaviour in tree.behaviours) {
         // at least name should go through, all others are optional
         node = _create_node({
@@ -442,11 +444,10 @@ var py_trees = (function() {
         if ( typeof tree.behaviours[behaviour].children !== 'undefined') {
             console.log("    Children: " + tree.behaviours[behaviour].children)
             tree.behaviours[behaviour].children.forEach(function (child_id, index) {
-                link = py_trees.create_link({
+                link = _create_link({
                     source: _nodes[tree.behaviours[behaviour].id],
                     target: _nodes[child_id],
                 })
-                _links.push(link)
                 link.addTo(graph)
             });
         }
@@ -470,13 +471,13 @@ var py_trees = (function() {
       attrs: {
           'box': {
               opacity: visited ? 1.0 : 0.3
-            
+
           },
           'type': {
               fill: colour || '#555555',
               opacity: visited ? 1.0 : 0.3
           },
-          
+
       }
     })
     var highlight_colours = {
@@ -493,8 +494,8 @@ var py_trees = (function() {
                 name: 'highlight',
                 args: {
                     color: highlight_colours[status],
-                    width: 2,
-                    opacity: 0.5,
+                    width: 3,
+                    opacity: 0.8,
                     blur: 5
                 }
             }
@@ -511,7 +512,7 @@ var py_trees = (function() {
       var link = new joint.shapes.standard.Link();
       link.source(source)
       link.target(target)
-      link.connector('smooth');
+      link.connector('smooth')
       return link
   }
 
@@ -543,7 +544,7 @@ var py_trees = (function() {
   }
 
   /**
-   * Initialise data for a panning maneuvre. 
+   * Initialise data for a panning maneuvre.
    */
   var _pan_canvas_begin = function(paper, event, x, y) {
       console.log("PanCanvasBegin")
@@ -556,7 +557,7 @@ var py_trees = (function() {
   }
   /**
    * Pan the canvas, lookup the offset via paper.translate() later
-   * (necessary when rendering the html views, but not the models). 
+   * (necessary when rendering the html views, but not the models).
    */
   var _pan_canvas_move = function(paper, event, x, y) {
       paper.translate(
@@ -566,7 +567,7 @@ var py_trees = (function() {
   }
   /**
    * Fit the tree to the canvas if scale < 1.0,
-   * otherwise just render it normally (scale: 1.0). 
+   * otherwise just render it normally (scale: 1.0).
    */
   var _fit_content_to_canvas = function(paper, event, x, y) {
       paper.scaleContentToFit({
@@ -584,6 +585,7 @@ var py_trees = (function() {
     create_link: _create_link,
     create_node: _create_node,
     fit_content_to_canvas: _fit_content_to_canvas,
+    foo: _foo,
     layout_graph: _layout_graph,
     pan_canvas_begin: _pan_canvas_begin,
     pan_canvas_move: _pan_canvas_move,

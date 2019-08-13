b0beb514b515c2634e6f267d096c250b178bcb6c
Daniel Stonier <d.stonier@gmail.com>
2019-08-12 23:01:48 -0400

    [infra] revert back to virtual env with pip installed dependencies
    Since the system dependencies are failing. This is somewhat
    more attractive than pipenv since it doesn't force installation of
    pip modules (namely pipenv) on the user.

--- virtualenv.bash
+++ virtualenv.bash
@@ -68,28 +68,46 @@ install_package ()
 
 ##############################################################################
 
-echo ""
-pretty_header "Using pipenv to facilitate the virtual environment"
-echo ""
+install_package virtualenvwrapper || return
 
-install_package python3-pip || return
+# To use the installed python3
+VERSION="--python=/usr/bin/python3"
+# To use a specific version
+# VERSION="--python=python3.6"
 
-PACKAGE_NAME=pipenv
-which ${PACKAGE_NAME} > /dev/null
-if [ $? -ne 0 ]; then
-  pip3 install --user pipenv
-else
-  pretty_print "  $(padded_message ${PACKAGE_NAME} "found")"
+if [ "${VIRTUAL_ENV}" == "" ]; then
+  workon ${NAME}
+  result=$?
+  if [ $result -eq 1 ]; then
+    mkvirtualenv ${VERSION} ${NAME}
+  fi
+  if [ $result -eq 127 ]; then
+    pretty_error "Failed to find virtualenvwrapper aliases: 1) re-log or 2) source virtualenvwrapper.sh in your shell's .rc"
+    return 1
+  fi
 fi
-if [ $? -ne 0 ]; then
-  pretty_print "  $(padded_message ${PACKAGE_NAME} "failed")"
-  return
-fi
+
+# Use the external pyqt5 rather than having to compile it in the virtual env
+#   Currently broken -> https://github.com/stuaxo/vext/issues/58
+#
+# install_package pyqt5-dev || return
+# install_package python3-pyqt5 || return
+# install_package python3-pyqt5.qtsvg || return
+# install_package python3-sip-dev || return
+# install_package pyqt5-dev-tools || return
+# install_package qttools5-dev-tools || return
+# install_package python3-pyqt5.qtwebengine || return
+# pip install vext.pyqt5
+
+# Alternatively, fetch the wheels
+#  Crashes on exit, https://github.com/splintered-reality/py_trees_js/issues/100
+pip install PyQt5
+pip install PyQtWebEngine
+
+python setup.py develop
 
 echo ""
-echo "Once entered, leave the virtual env with 'exit'"
+echo "Leave the virtual environment with 'deactivate'"
 echo ""
 echo "I'm grooty, you should be too."
 echo ""
-
-pipenv shell

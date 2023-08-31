7c5ea746bd047fd7790b6fcbe1658dd21009643a
Daniel Stonier <d.stonier@gmail.com>
2023-08-31 13:42:04 +0000

    [formatter] reformat auto-generated resources.py

--- py_trees_js/resources.py
+++ py_trees_js/resources.py
@@ -28404,7 +28404,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x85\x4a\xad\x34\x49\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+qt_version = [int(v) for v in QtCore.qVersion().split(".")]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -28414,11 +28414,15 @@ else:
 
 
 def qInitResources():
-    QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
 
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
 
 
 qInitResources()
